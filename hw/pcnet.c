91c15bfaec1764ce2896a393eabee1183afe1130
Ian Jackson <ian.jackson@eu.citrix.com>
2015-12-09 11:47:35 +0000

    net: pcnet: add check to validate receive data size(CVE-2015-7504)
    In loopback mode, pcnet_receive routine appends CRC code to the
    receive buffer. If the data size given is same as the buffer size,
    the appended CRC code overwrites 4 bytes after s->buffer. Added a
    check to avoid that.
    
    This is XSA-162.
    
    Reported-by: Qinghao Tang <luodalongde@gmail.com>
    Signed-off-by: Prasad J Pandit <pjp@fedoraproject.org>

--- hw/pcnet.c
+++ hw/pcnet.c
@@ -1153,7 +1153,7 @@ static void pcnet_receive(void *opaque, const uint8_t *buf, int size)
                 uint32_t fcs = ~0;
                 uint8_t *p = src;
 
-                while (p != &src[size-4])
+                while (p != &src[size])
                     CRC(fcs, *p++);
                 crc_err = (*(uint32_t *)p != htonl(fcs));
             }
@@ -1284,12 +1284,13 @@ static void pcnet_transmit(PCNetState *s)
         bcnt = 4096 - GET_FIELD(tmd.length, TMDL, BCNT);
 
         /* if multi-tmd packet outsizes s->buffer then skip it silently.
-           Note: this is not what real hw does */
-        if (s->xmit_pos + bcnt > sizeof(s->buffer)) {
-           s->xmit_pos = -1;
-           goto txdone;
+         * Note: this is not what real hw does.
+         * Last four bytes of s->buffer are used to store CRC FCS code.
+         */
+        if (s->xmit_pos + bcnt > sizeof(s->buffer) - 4) {
+            s->xmit_pos = -1;
+            goto txdone;
         }
-
         s->phys_mem_read(s->dma_opaque, PHYSADDR(s, tmd.tbadr),
                          s->buffer + s->xmit_pos, bcnt, CSR_BSWP(s));
         s->xmit_pos += bcnt;
